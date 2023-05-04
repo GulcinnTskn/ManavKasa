60d7f4a5c1ed8f63c338c6ec8746ea0a33f78259
Gülçin <gulcintskn18@gmail.com>
2023-05-04 18:20:18 +0300

    Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran program

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       double armut = 2.14,elma=3.67,domates=1.11,muz=0.95,patlican=5.00,toplam,armutKilo,elmaKilo,domatesKilo,muzKilo,patlicanKilo;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner input = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Armut Kaç Kilo : ");
+        armutKilo = input.nextDouble();
+
+        System.out.print("Elma Kaç Kilo : ");
+        elmaKilo = input.nextDouble();
+
+        System.out.print("Domates Kaç Kilo : ");
+        domatesKilo = input.nextDouble();
+
+        System.out.print("Muz Kaç Kilo : ");
+        muzKilo = input.nextDouble();
+
+        System.out.print("Patlıcan Kaç Kilo : ");
+        patlicanKilo = input.nextDouble();
+
+        toplam = (armut*armutKilo)+(elma*elmaKilo)+(domates*domatesKilo)+(muz*muzKilo)+(patlican*patlicanKilo);
+        System.out.print("Toplam Tutar : " +toplam + " TL");
     }
 }
